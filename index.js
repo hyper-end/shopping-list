4c2b79424701e871bf73fefadd716369226d89af
desmati <desmati@gmail.com>
2023-04-04 09:54:46 +0200

    database connection and simple api backend

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(
+	`postgres://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBURL}:${process.env.DBPORT}/${process.env.DBNAME}`
+);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+const Task = sequelize.define("Task", {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: DataTypes.UUIDV4,
+		primaryKey: true,
+	},
+	title: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	completed: {
+		type: DataTypes.BOOLEAN,
+	},
 });
 
-app.get('/test', (req, res) => {
-    res.send('hello test')
+const main = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+		// await sequelize.sync({ force: true });
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+};
+
+app.use(morgan("tiny"));
+
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/tasks", async (req, res) => {
+	const tasks = await Task.findAll();
+	res.json(tasks.map((t) => t.toJSON()));
+
+	console.log(tasks);
 });
 
-app.listen(8080);
+app.post("/task", async (req, res) => {
+	const { title, completed } = req.body;
+	const task = await Task.create({ title, completed });
+	res.json(task.toJSON());
+});
+
+app.patch("/task/:id", async (req, res) => {
+	const { completed } = req.body;
+	const { id } = req.params;
+	await Task.update({ completed }, { where: { id } });
+	res.end();
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
+
+main();
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('hello world')
+// });
+
+// app.get('/test', (req, res) => {
+//     res.send('hello test')
+// });
+
+// app.listen(8080);
 
